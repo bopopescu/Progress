4b104651486b77ffb318f9ffb8f5b8547a7a5f04
jun29 <jun0814soccer@gmail.com>
2019-01-10 14:30:30 +0900

    家具・移動ページ作成

---
+++ htdocs/Progress/wp-content/themes/progresstheme/page-assemble.php
@@ -0,0 +1,155 @@
+<?php
+/*
+Template Name: assemble
+*/
+get_header();
+?>
+    <aside> 
+        <?php get_sidebar(); //sidebar.phpを取得 ?>
+    </aside>
+    <section class="container">
+        <div class="section-title">
+            <h4 class="char-color">assemble</h4>
+            <div class="title">
+                <h1>家具<span class="char-color">組</span>立・移動<span class="sm-char">サービス</span></h1>
+            </div>
+            <h4 class="char-color">基本的には、無償で行います。</h4>
+            <p>不用品回収リングでのお引越しの際や、不用品回収をご依頼のタイミングで、新しい家具を買い換えた場合などには、家具の組立・<br>移動を弊社におまかせください。大きい家具の移動や組立はとても大変なこと！お困りのお客様が多くいらっしゃるかと思います。<br>そんなときにはご自身で無理をせず、ぜひぜひご相談を！！また、移動や組立だけを頼みたいという方でも、お気軽にお問い合わせく<br>ださい。お待ちしております！
+            </p>
+        </div>
+        <section class="multi top-title">
+            <div>
+                <h2>大切な家具を迅速・丁寧に移動・組立します。</h2>
+            </div>
+            <div class="row" id="assemble-row">
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/bed.jpg" alt=""></div>
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/tana.jpg" alt=""></div>
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/desk.jpg" alt=""></div>
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/tansu.jpg" alt=""></div>
+            </div>
+            <div>
+                <p>様々な家具の移動・組立に対応しております。</p>
+            </div>
+            <div class="multi-column">
+                <div class="multi-list">
+                    <div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/kumitate.jpg" alt="">
+                    </div>
+                    <h2>家具の組立て</h2>
+                    <p>ベット・棚・ラックなどあらゆる家具の組立に対応いたします。例えば弊社サービスをご依頼のタイミングでお申し付けいただければ、不用品回収から引越し・組立とまとめて同じ日程で進めることができ、とても便利でお得です！</p>
+                </div>
+                <div class="multi-list up thin">
+                    <div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/idou.jpg" alt="">
+                    </div>
+                    <h2>家具の移動</h2>
+                    <p>こちらには家具移動にも経験をもつプロ達がそろっており、大事な家具移動に細心の注意を払って素早く丁寧にお運びいたします！責任を持って作業にかからせていただきますのでご安心ください！少しでもお客様のお力になれることを願っております</p>
+                </div>
+                <div class="multi-list">
+                    <div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/tsuri.jpg" alt="">
+                    </div>
+                    <h2>吊作業での搬出</h2>
+                    <p>「玄関からベットがでない」そんな時にも安心。窓から搬出する吊作業にも慣れた熟練スタッフにおまかせください。</p>
+                </div>
+                <div class="multi-list up thin">
+                    <div>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/kaitai.jpg" alt="">
+                    </div>
+                    <h2>分解・解体・撤去</h2>
+                    <p>物置の解体や家具の分解もOK！不用品と物置まるごとも対応可能です。リフォーム等もご相談を。</p>
+                </div>
+            </div>
+        </section>
+    </section>
+    <!-- ====================================================================== -->
+    <section class="container-sp">
+        <div class="section-title">
+            <div class="title">
+                <h1>引<span class="char-color">越</span>しサービス</h1>
+            </div>
+            <span>move house</span>
+            <span class="line"></span>
+            <h4 class="char-color">大切な家具を迅速・丁寧に移動・組立します。</h4>
+        </div>
+        <section class="multi-sp" id="assemble-block-pic">
+            <div class="row">
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/bed_sp.jpg" alt=""></div>
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/tana_sp.jpg" alt=""></div>
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/desk_sp.jpg" alt=""></div>
+                <div><img src="<?php echo get_template_directory_uri(); ?>/img/assemble/tansu_sp.jpg" alt=""></div>
+            </div>
+        </section>
+        <div>
+            <p>様々な家具の移動・組立に対応しております。</p>
+        </div>
+        <section class="multi-sp">
+            <div class="multi-column-sp" id="reform-sp">
+                <div class="multi-list-sp">
+                    <div class="row">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/kumitate_sp.jpg" alt="">
+                        </div>
+                        <div class="reform-text">
+                            <h4>分解・解体・組み立て</h4>
+                            <p>ベット・棚・ラックなどあらゆる家具の組立に対応いたします。</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="multi-list-sp">
+                    <div class="row">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/idou_sp.jpg" alt="">
+                        </div>
+                        <div class="reform-text">
+                            <h4>家具の移動</h4>
+                            <p>家具移動にも経験をもつプロ達がそろっており、大事な家具移動に細心の注意を払って素早く丁寧にお運びいたします！</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="multi-list-sp">
+                    <div class="row">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/tsuri_sp.jpg" alt="">
+                        </div>
+                        <div class="reform-text">
+                            <h4>吊作業での搬出</h4>
+                            <p>「玄関からベットがでない」そんな時にも安心。窓から搬出する吊作業にも慣れた熟練スタッフにおまかせください。</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="multi-list-sp">
+                    <div class="row">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/assemble/kaitai_sp.jpg" alt="">
+                        </div>
+                        <div class="reform-text">
+                            <h4>分解・解体・撤去</h4>
+                            <p>物置の解体や家具の分解もOK！不用品と物置まるごとも対応可能です。リフォーム等もご相談を。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <section class="card">
+        <span>各種クレジットカードもご利用いただけます</span>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/card_sp.png" alt="">
+    </section>
+    <section class="banner">
+        <ul class="slider">
+            <li>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/banner_mitsui.jpg" alt="">
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/banner_STAFF.jpg" alt="">
+            </li>
+            <li>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/banner_VOICE.jpg" alt="">
+            </li>
+        </ul>
+    </section>
+    <div class="guideLine">
+        <span>不用品回収リング TOPページ</span>
+    </div>
+<?php get_footer(); ?>
